a5f9bad0d33ead30858ca6a95e4f704a569d8c65
Ken Kousen <ken.kousen@kousenit.com>
2025-08-13 14:09:36 -0400

    Migrate controller MVC test to @MockitoBean
    Adopt Spring’s new Mockito-based bean override in @WebMvcTest to replace deprecated @MockBean.

--- src/test/java/com/kousenit/springgpt5/AiControllerTest.java
+++ src/test/java/com/kousenit/springgpt5/AiControllerTest.java
@@ -2,10 +2,9 @@ package com.kousenit.springgpt5;
 
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
-import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.WebMvcTest;
-import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.test.context.bean.override.mockito.MockitoBean;
 import org.springframework.http.MediaType;
 import org.springframework.test.web.servlet.MockMvc;
 
@@ -23,7 +22,7 @@ class AiControllerTest {
     @Autowired
     private MockMvc mockMvc;
 
-    @MockBean
+    @MockitoBean
     private MyAiService myAiService;
 
     @Test
